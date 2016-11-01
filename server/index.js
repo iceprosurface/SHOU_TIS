d95d27877cb46fbe1c96db83680ee8cbb2b82a78
icepro <775846939@qq.com>
2016-11-01 10:33:36 +0800

    v 0.0.2 补全一个错误处理的情况，添加500和404响应回执 #2

--- server/index.js
+++ server/index.js
@@ -29,6 +29,21 @@ app.use(session({
     }
 }));
 
-require(__dirname + '/lib/controller')(app, { islog: true });
+require(__dirname + '/lib/controller')(app, {
+    islog: true
+});
 
+// 错误处理
+app.use(function(err, req, res, next) {
+    // 系统错误这时候输出错误
+    if (!module.parent) console.error(err.stack);
+
+    // 输出错误
+    res.status(500).send("server error");
+});
+
+// 显然当没有任何匹配的时候，显示404即可
+app.use(function(req, res, next) {
+    res.status(404).send("file or api not find in server and path is : " + req.originalUrl);
+});
 app.listen(8888);
