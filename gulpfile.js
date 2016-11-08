2175b625e58b7fab12b917ce9f805c9586101b47
icepro <775846939@qq.com>
2016-11-08 21:21:49 +0800

    v 0.0.2 修改使用webbrowfy

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,6 @@ var gutil = require('gulp-util');
 var browserSync = require('browser-sync').create();
 //简化reload
 var reload = browserSync.reload;
-// es6->es5
-var babel = require("gulp-babel");
 //html引入
 var fileinclude = require('gulp-file-include');
 //hash
@@ -27,42 +25,69 @@ var rev = require('gulp-rev-append');
 var uglify = require('gulp-uglify');
 //增量更新
 var changed = require('gulp-changed');
+// require 自动依赖
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
+var buffer = require('vinyl-buffer');
+var watchify = require('watchify');
 
 const apiProxy = proxy('/api/', {
-	target: 'localhost:8888',
+    target: 'localhost:8888',
     changeOrigin: true,
-	router: {
-		'localhost:3000' : 'http://localhost:8888'
-	}
+    router: {
+        'localhost:3000': 'http://localhost:8888'
+    }
 });
 
-gulp.task('default', ['server','buildlib']);
+gulp.task('default', ['server', 'buildlib']);
 
-gulp.task('buildlib',function(){
-	return gulp.src('src/lib/**/**')
-		.pipe(gulp.dest('dest/lib'));
+gulp.task('buildlib', function() {
+    return gulp.src('src/lib/**/**')
+        .pipe(gulp.dest('dest/lib'));
 });
+
 gulp.task('sass', function() {
     return gulp.src("src/sass/main.scss")
-        .pipe(sass({ style: 'expanded' }))
+        .pipe(sass({
+            style: 'expanded'
+        }))
         .on('error', function(err) {
             gutil.log('sass Error!', err.message);
             this.emit('end');
         })
         .pipe(gulp.dest("dest/css"))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(minifycss({ keepSpecialComments: "*", processImportFrom: ['!fonts.googleapis.com'] }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss({
+            keepSpecialComments: "*",
+            processImportFrom: ['!fonts.googleapis.com']
+        }))
         .pipe(gulp.dest("dest/css"))
-        .pipe(reload({ stream: true }));
+        .pipe(reload({
+            stream: true
+        }));
 });
+
 gulp.task('css', function() {
     return gulp.src("src/css/**/*.css")
-        .pipe(sass({ style: 'expanded' }))
+        .pipe(sass({
+            style: 'expanded'
+        }))
         .pipe(gulp.dest("dest/css"))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(minifycss({ keepSpecialComments: "*", processImportFrom: ['!fonts.googleapis.com'] }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss({
+            keepSpecialComments: "*",
+            processImportFrom: ['!fonts.googleapis.com']
+        }))
         .pipe(gulp.dest("dest/css"))
-        .pipe(reload({ stream: true }));
+        .pipe(reload({
+            stream: true
+        }));
 });
 gulp.task('html', function() {
     gulp.src("src/**/*.html")
@@ -78,68 +103,82 @@ gulp.task('html', function() {
             this.emit('end');
         })
         .pipe(gulp.dest("dest"))
-        .pipe(reload({ stream: true }));
+        .pipe(reload({
+            stream: true
+        }));
 });
 gulp.task('script', function() {
-    return gulp.src(["src/js/**/*.js","src/js/**/*.jsx"])
-        .pipe(changed("dest/js"))
-        .pipe(gulp.dest("dest/js"))
-        .pipe(babel({
-            presets: ['es2015','react']
+    return watchify(browserify({
+            entries: ["./src/js/main.jsx"],
         }))
+        .transform(babelify, {
+            presets: ["es2015", "react"],
+        })
+        .bundle()
         .on('error', function(err) {
             gutil.log('js Error!', err.message);
             this.emit('end');
         })
-        .pipe(uglify())
-        .on('error', function(err) {
-            gutil.log('js Error!', err.message);
-            this.emit('end');
-        })
-        .pipe(rename({ suffix: '.min' }))
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
         .pipe(gulp.dest("dest/js"))
-        .pipe(reload({ stream: true }));
+//        .pipe(uglify())
+//        .on('error', function(err) {
+//            gutil.log('js Error!', err.message);
+//            this.emit('end');
+//        })
+//        .pipe(rename({
+//            suffix: '.min'
+//        }))
+        .pipe(gulp.dest("dest/js"))
+        .pipe(reload({
+            stream: true
+        }));
 });
 // 浏览器重载
 gulp.task('script-watch', ['script'], reload);
 // gulp.task('html-watch', ['html'], reload);
 
 // 静态服务器
-gulp.task('server', ['html', 'sass', 'script', 'css','serve'], function() {
+gulp.task('server', ['html', 'sass', 'script', 'css', 'serve'], function() {
     // 从这个项目的根目录启动服务器
     browserSync.init({
-		proxy: "localhost:8888",  // local node app address
+        proxy: "localhost:8888", // local node app address
         //禁止网络模式
         online: false,
-		//静止ui模式
-		ui: false,
-		index: 'index.html'
+        //静止ui模式
+        ui: false,
+        index: 'index.html'
     });
     // 添加 browserSync.reload 到任务队列里
     // 所有的浏览器重载后任务完成。
     watch("src/sass/**/*.scss", function() { //监听所有sass
         gulp.start('sass'); //出现修改、立马执行sass任务
     });
-    watch(["src/js/**/*.js","src/js/**/*.jsx"], function() { //监听所有js
+    watch(["src/js/**/*.js", "src/js/**/*.jsx"], function() { //监听所有js
         gulp.start('script-watch'); //出现修改、立马执行js任务
     });
-    watch(["src/**/*.html","!src/public/*.html"], function() { //监听所有html
+    watch(["src/**/*.html"], function() { //监听所有html
         gulp.start('html').on('change', reload); //出现修改、立马执行html任务
     });
     watch("src/css/**/*.css", function() { //监听所有css
         gulp.start('css'); //出现修改、立马执行css任务
     });
 });
-
+// node 服务器
 gulp.task('serve', function() {
-    var server = gls.new('./server/index.js',{env: {NODE_ENV: 'development'}});
-	server.start().then(function(result) {
+    var server = gls.new('./server/index.js', {
+        env: {
+            NODE_ENV: 'development'
+        }
+    });
+    server.start().then(function(result) {
         console.log('Server exited with result:', result);
     });
-	watch('./server/**/*.js', function (file) {
-      server.start.apply(server);
-      server.notify.apply(server, [file]);
-    })
+    watch('./server/**/*.js', function(file) {
+        server.start.apply(server);
+        server.notify.apply(server, [file]);
+    });
 });
 
 gulp.task('watch', function() {
